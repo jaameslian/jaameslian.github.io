0d1662dc1052f8f27ebe498c8f0e16811f5540fb
jaameslian <156141320+jaameslian@users.noreply.github.com>
2025-04-22 13:49:40 -0500

    Commit #3
    Created a mini summary of me portion with pfp

--- components/core.js
+++ components/core.js
@@ -7,15 +7,45 @@ var coreCss = document.createElement("link");
 coreCss.href = "/components/css/core.css"
 coreCss.rel = "stylesheet"
 
+var googleIcons = document.createElement("link");
+googleIcons.href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=menu"
+googleIcons.rel = "stylesheet"
+
+
+head.appendChild(googleIcons)
 head.appendChild(coreCss)
 
-if(nav) {
-    const navComponent = fetch("./components/html/nav.html")
-    .then(response => response.text())
-    .then(data => {
-        nav.innerHTML = data
-        console.log(data)
-    });
+await loadNav() 
 
-    nav.innerHTML = navComponent
+async function loadNav() {
+    if(nav) {
+        var navComponent = await fetch("./components/html/nav.html")
+
+        const navData = await navComponent.text();
+        // .then(response => response.text())
+        // .then(data => {
+        //     nav.innerHTML = data
+        //     console.log(data)
+        // });
+    
+        nav.innerHTML = navData
+    }
 }
+
+const mobileUIHider = document.getElementById("mobileUlClose")
+
+// document.getElementById("mobileUlClose").addEventListener("click", () => {
+//     if(document.getElementById("mobileUlClose").classList.contains("navClosed")) {
+//         document.getElementById("mobileUlClose").classList.remove("navClosed")
+//     }
+
+//     document.getElementById("mobileUlClose").classList.add("navClosed")
+// })
+
+document.getElementById("mobileMenu").addEventListener("click", () => {
+    if(!mobileUIHider.classList.contains("navClosed") && !(mobileUIHider.classList.contains("navOpened"))) {
+        mobileUIHider.classList.add("navOpened")
+    } else if(mobileUIHider.classList.contains("navOpened")) {
+        mobileUIHider.classList.remove("navOpened")
+    }
+})
